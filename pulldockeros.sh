8131cecdce730993adb2fc5296cbdc1c22c5e69b
James Shane <jamesshane@gmail.com>
2020-03-17 16:09:09 -0600

    misc

--- pulldockeros.sh
+++ pulldockeros.sh
@@ -5,6 +5,6 @@ docker login
 docker pull jamesshane/docker-vscode
 docker pull jamesshane/docker-firefox
 docker pull jamesshane/docker-ubuntux
-docker pull jamesshane/docker-ubuntuvnc
-docker pull jamesshane/docker-discord
+#docker pull jamesshane/docker-ubuntuvnc
+#docker pull jamesshane/docker-discord
 
