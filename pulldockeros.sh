ca10e5f4800430744df18c17ea86f8fd83b4e2bd
James Shane <jamesshane@gmail.com>
2020-03-17 09:10:56 -0600

    added pulldockeros.sh to pull repos, not build.

---
+++ pulldockeros.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker login
+
+docker pull jamesshane/docker-vscode
+docker pull jamesshane/docker-firefox
+docker pull jamesshane/docker-ubuntux
+docker pull jamesshane/docker-ubuntuvnc
+docker pull jamesshane/docker-discord
+
