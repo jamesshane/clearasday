aeb238eef16e73f87c507c3e282f082f61ef31dc
James Shane <jamesshane@gmail.com>
2020-03-19 03:26:39 -0600

    added getdressed for bash and vim

---
+++ getdressed.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" && \
+sed -i 's/font/powerline/g' ~/.bashrc
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
+echo -e "\nsyntax off\nexecute pathogen#infect()\n" >> ~/.vimrc
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+
+cd ~/.vim/bundle && \
+git clone https://github.com/scrooloose/nerdtree
+
+cd ~/.vim/bundle && \
+git clone https://github.com/tpope/vim-fugitive
+
+cd ~/.vim/bundle && \
+git clone https://github.com/nathanaelkane/vim-indent-guides
+
+cd ~/.vim/bundle && \
+git clone https://github.com/kien/rainbow_parentheses.vim
+
+cd ~/.vim/bundle && \
+git clone https://github.com/sjl/badwolf
+
+
--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ sudo swupd bundle-add os-utils-gui && \
 sudo swupd bundle-add dev-utils-gui && \
 sudo swupd bundle-add  desktop-awesome && \
 sudo install-vbox-lga && \
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" && \
-sed -i 's/font/powerline/g' ~/.bashrc && \
 sudo swupd bundle-add containers-basic && \
 sudo usermod -aG docker j
 sudo timedatectl set-timezone America/Denver
